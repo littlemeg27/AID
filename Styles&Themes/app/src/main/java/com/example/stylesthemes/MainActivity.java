f02381774afe48c931cdf4e9da2e916b50b3beb4
Brenna Pavlinchak <littlemeg27@student.fullsail.edu>
2025-02-23 19:57:29 -0500

    Finished project 3

--- Styles&Themes/app/src/main/java/com/example/stylesthemes/MainActivity.java
+++ Styles&Themes/app/src/main/java/com/example/stylesthemes/MainActivity.java
@@ -7,7 +7,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,13 +23,6 @@ public class MainActivity extends AppCompatActivity
     private EditText editText;
     private RadioGroup backgroundColorGroup, textColorGroup;
     private SwitchCompat boldSwitch;
-    private RadioButton colorRed;
-    private RadioButton colorBlue;
-    private RadioButton colorGreen;
-    private RadioButton colorBlack;
-    private RadioButton textColorRed;
-    private RadioButton textColorBlue;
-    private RadioButton textColorBlack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -45,7 +37,6 @@ public class MainActivity extends AppCompatActivity
         {
             setContentView(R.layout.activity_main_portrait);
         }
-
         frameLayout = findViewById(R.id.frameLayout);
         frameText = findViewById(R.id.frameText);
         editText = findViewById(R.id.editText);
@@ -53,63 +44,46 @@ public class MainActivity extends AppCompatActivity
         textColorGroup = findViewById(R.id.textColorGroup);
         boldSwitch = findViewById(R.id.boldSwitch);
 
-        RadioButton colorRed = findViewById(R.id.colorRed);
-        RadioButton colorBlue = findViewById(R.id.colorBlue);
-        RadioButton colorGreen = findViewById(R.id.colorGreen);
-        RadioButton textColorRed = findViewById(R.id.textColorRed);
-        RadioButton textColorBlue = findViewById(R.id.textColorBlue);
-        RadioButton textColorBlack = findViewById(R.id.textColorBlack);
-
         backgroundColorGroup.setOnCheckedChangeListener((group, checkedId) ->
         {
-            if (checkedId == R.id.colorRed)
+            int color = getResources().getColor(
+                    checkedId == R.id.colorRed ? R.color.red :
+                            checkedId == R.id.colorBlue ? R.color.blue :
+                                    R.color.green, getTheme());
+
+            frameLayout.setBackgroundColor(color);
+
+            if (getSupportActionBar() != null)
             {
-                frameLayout.setBackgroundColor(getResources().getColor(R.color.red, getTheme()));
+                getSupportActionBar().setBackgroundDrawable(
+                        new android.graphics.drawable.ColorDrawable(color));
             }
-            else if (checkedId == R.id.colorBlue)
-            {
-                frameLayout.setBackgroundColor(getResources().getColor(R.color.blue, getTheme()));
-            }
-            else if (checkedId == R.id.colorGreen)
-            {
-                frameLayout.setBackgroundColor(getResources().getColor(R.color.green, getTheme()));
-            }
+
+            getWindow().setStatusBarColor(color);
         });
 
         textColorGroup.setOnCheckedChangeListener((group, checkedId) ->
-        {
-            if (checkedId == R.id.textColorRed)
-            {
-                frameText.setTextColor(getResources().getColor(R.color.red, getTheme()));
-            }
-            else if (checkedId == R.id.textColorBlue)
-            {
-                frameText.setTextColor(getResources().getColor(R.color.blue, getTheme()));
-            }
-            else if (checkedId == R.id.textColorBlack)
-            {
-                frameText.setTextColor(getResources().getColor(R.color.black, getTheme()));
-            }
-        });
+                frameText.setTextColor(getResources().getColor(
+                        checkedId == R.id.textColorRed ? R.color.red :
+                                checkedId == R.id.textColorBlue ? R.color.blue :
+                                        R.color.green, getTheme())));
 
         editText.addTextChangedListener(new TextWatcher()
         {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int start, int before, int count)
+            public void onTextChanged(CharSequence s, int start, int before, int count)
             {
-                frameText.setText(charSequence);
+                frameText.setText(s);
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {}
+            public void afterTextChanged(Editable s) {}
         });
 
         boldSwitch.setOnCheckedChangeListener((buttonView, isChecked) ->
-        {
-            frameText.setTypeface(null, isChecked ? Typeface.BOLD : Typeface.NORMAL);
-        });
+                frameText.setTypeface(null, isChecked ? Typeface.BOLD : Typeface.NORMAL));
     }
 }
